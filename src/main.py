99ea22e14ac8b16a235e8b08801af32bc2923c30
limitcantcode <limit@limit-cc.com>
2025-04-07 03:46:21 +0200

    [feature] Added better chat context
    Updated endpoints with new jaison-core v2.1.0.
    
    Updated chat context to provide summaries instead of a script.
    
    Added ability for chat to converse directly with jaison-core with
    different chat modes.
    
    Signed-off-by: limitcantcode <limit@limit-cc.com>

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-from utils.args import args
+
 from dotenv import load_dotenv
+import asyncio
+from utils.args import args
 load_dotenv(dotenv_path=args.env)
 
 from utils.twitch_monitor import TwitchContextMonitor
 
 twitch = TwitchContextMonitor()
-twitch.run()
+event_loop = asyncio.new_event_loop()
+asyncio.set_event_loop(event_loop)
+event_loop.create_task(twitch.run())
+event_loop.run_forever()
